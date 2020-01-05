8e1eb83295e7e6ba8da39f396faf721a3bb26720
Mariano Mangialavori <mariann_03@outlook.com>
2020-01-05 19:59:15 -0300

    fix multiple requests error

--- api/scores.js
+++ api/scores.js
@@ -1,12 +1,36 @@
 const fetch = require('node-fetch')
 
+async function fetchWithRetry(id, index) {
+	let res = await fetch(`https://api.jikan.moe/v3/anime/${id}/`)
+	let retries = 0
+	while (res.status !== 404 && res.status !== 200 && retries < 5) {
+		res = await new Promise(resolve => {
+			setTimeout(async () => resolve(await fetch(`https://api.jikan.moe/v3/anime/${id}/`)), 500 * retries * index)
+		})
+		retries++
+	}
+	console.log(res.status)
+	return res
+}
+
 module.exports = async (req, res) => {
-	const promises = req.body.ids.map(async id => {
-		const res = await fetch(`https://api.jikan.moe/v3/anime/${id}/`)
-		const { score } = await res.json()
-		return score
+	const { ids } = req.body
+	const promises = ids.map(async (id, index) => {
+		try {
+			let res = await fetchWithRetry(id, index)
+			const { score } = await res.json()
+			return score || 'NOT FOUND'
+		} catch (error) {
+			console.log(error)
+			return 'NOT FOUND'
+		}
 	})
 
 	const scores = await Promise.all(promises)
-	res.status(200).send(scores)
+	const scoresByIds = ids.reduce((acc, id, index) => {
+		acc[id] = scores[index]
+		return acc
+	}, {})
+
+	res.status(200).send(scoresByIds)
 }
